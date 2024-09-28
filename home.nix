73e09257ac2881191103a4b59293226f173049f6
daniel-jeremia <daniel.jeremia@datasaur.ai>
2024-09-28 00:44:18 -0700

    NixOS initial commit

---
+++ home.nix
@@ -0,0 +1,435 @@
+{
+  # FIXME: uncomment the next line if you want to reference your GitHub/GitLab access tokens and other secrets
+  # secrets,
+  pkgs,
+  username,
+  nix-index-database,
+  config,
+  ...
+}: let
+  unstable-packages = with pkgs.unstable; [
+    # FIXME: select your core binaries that you always want on the bleeding-edge
+    atac
+    awscli2
+    azure-cli
+    bat
+    bottom
+    busybox
+    coreutils
+    curl
+    du-dust
+    fd
+    findutils
+    fx
+    gcc
+    git
+    git-crypt
+    gnumake
+    google-cloud-sdk
+    htop
+    jq
+    k9s
+    killall
+    kubectl
+    lazygit
+    mosh
+    nodejs_22
+    procs
+    ripgrep
+    sd
+    sshs
+    stow
+    tmux
+    tree
+    typescript
+    unzip
+    vim
+    neovim
+    wget
+    wslu
+    yazi
+    zip
+  ];
+
+  stable-packages = with pkgs; [
+    # FIXME: customize these stable packages to your liking for the languages that you use
+
+    # FIXME: you can add plugins, change keymaps etc using (jeezyvim.nixvimExtend {})
+    # https://github.com/LGUG2Z/JeezyVim#extending
+    # jeezyvim
+
+    # key tools
+    gh # for bootstrapping
+    just
+
+    # core languages
+    rustup
+
+    # rust stuff
+    cargo-cache
+    cargo-expand
+
+    # local dev stuf
+    mkcert
+    httpie
+
+    # treesitter
+    tree-sitter
+
+    # language servers
+    nodePackages.vscode-langservers-extracted # html, css, json, eslint
+    nodePackages.yaml-language-server
+    nil # nix
+
+    # formatters and linters
+    alejandra # nix
+    deadnix # nix
+    nodePackages.prettier
+    shellcheck
+    shfmt
+    statix # nix
+  ];
+in {
+  imports = [
+    nix-index-database.hmModules.nix-index
+    ./kickstart.nixvim/nixvim.nix
+  ];
+
+  home.stateVersion = "22.11";
+
+  home = {
+    username = "${username}";
+    homeDirectory = "/home/${username}";
+
+    sessionVariables.EDITOR = "nvim";
+    # FIXME: set your preferred $SHELL
+    sessionVariables.SHELL = "/etc/profiles/per-user/${username}/bin/zsh";
+
+    sessionVariables.WSL_INTEROP = "/run/WSL/interop";
+  };
+
+  home.packages =
+    stable-packages
+    ++ unstable-packages
+    ++
+    # FIXME: you can add anything else that doesn't fit into the above two lists in here
+    [
+      # pkgs.some-package
+      # pkgs.unstable.some-other-package
+    ];
+
+  programs = {
+    home-manager.enable = true;
+    nix-index.enable = true;
+    nix-index.enableFishIntegration = true;
+    nix-index.enableZshIntegration = true;
+    nix-index-database.comma.enable = true;
+
+    atuin.enable = true;
+
+    # FIXME: disable this if you don't want to use the starship prompt
+    starship.enable = true;
+    starship.settings = {
+      aws.disabled = true;
+      gcloud.disabled = true;
+      kubernetes.disabled = false;
+      git_branch.style = "242";
+      directory.style = "blue";
+      directory.truncate_to_repo = false;
+      directory.truncation_length = 8;
+      python.disabled = true;
+      ruby.disabled = true;
+      hostname.ssh_only = false;
+      hostname.style = "bold green";
+    };
+
+    # FIXME: disable whatever you don't want
+    fzf.enable = true;
+    fzf.enableFishIntegration = true;
+    fzf.enableZshIntegration = true;
+    lsd.enable = true;
+    lsd.enableAliases = true;
+    zoxide.enable = true;
+    zoxide.enableZshIntegration = true;
+    zoxide.enableFishIntegration = true;
+    zoxide.options = ["--cmd cd"];
+    broot.enable = true;
+    broot.enableZshIntegration = true;
+    broot.enableFishIntegration = true;
+    direnv.enable = true;
+    direnv.nix-direnv.enable = true;
+    direnv.enableZshIntegration = true;
+
+    git = {
+      enable = true;
+      package = pkgs.unstable.git;
+      delta.enable = true;
+      delta.options = {
+        line-numbers = true;
+        side-by-side = true;
+        navigate = true;
+      };
+      userEmail = "daniel.jeremia@datasaur.ai"; # FIXME: set your git email
+      userName = "daniel-jeremia"; #FIXME: set your git username
+      extraConfig = {
+        # FIXME: uncomment the next lines if you want to be able to clone private https repos
+        # url = {
+        #   "https://oauth2:${secrets.github_token}@github.com" = {
+        #     insteadOf = "https://github.com";
+        #   };
+        #   "https://oauth2:${secrets.gitlab_token}@gitlab.com" = {
+        #     insteadOf = "https://gitlab.com";
+        #   };
+        # };
+        push = {
+          default = "current";
+          autoSetupRemote = true;
+        };
+        merge = {
+          conflictstyle = "diff3";
+        };
+        diff = {
+          colorMoved = "default";
+        };
+      };
+    };
+
+    zsh = {
+      enable = true;
+      autocd = true;
+      autosuggestion.enable = true;
+      enableCompletion = true;
+      defaultKeymap = "emacs";
+      history.size = 10000;
+      history.save = 10000;
+      history.expireDuplicatesFirst = true;
+      history.ignoreDups = true;
+      history.ignoreSpace = true;
+      historySubstringSearch.enable = true;
+
+      plugins = [
+        {
+          name = "fast-syntax-highlighting";
+          src = "${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions";
+        }
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.5.0";
+            sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+          };
+        }
+        {
+          name = "vi-mode";
+          src = pkgs.zsh-vi-mode;
+          file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
+        }
+      ];
+
+      shellAliases = {
+        "..." = "./..";
+        "...." = "././..";
+        gc = "nix-collect-garbage --delete-old";
+        refresh = "source ${config.home.homeDirectory}/.zshrc";
+        show_path = "echo $PATH | tr ':' '\n'";
+        
+        update = "sudo nixos-rebuild switch --flake ~/configuration";
+
+        # FIXME: add more git aliases here if you want them
+        gapa = "git add --patch";
+        grpa = "git reset --patch";
+        gst = "git status";
+        gdh = "git diff HEAD";
+        gp = "git push";
+        gph = "git push -u origin HEAD";
+        gco = "git checkout";
+        gcob = "git checkout -b";
+        gcm = "git checkout master";
+        gcd = "git checkout develop";
+        v = "nvim";
+
+        pbcopy = "/mnt/c/Windows/System32/clip.exe";
+        pbpaste = "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -command 'Get-Clipboard'";
+        explorer = "/mnt/c/Windows/explorer.exe";
+      };
+
+      envExtra = ''
+        export PATH=$PATH:$HOME/.local/bin
+        export PATH=/mnt/c/Users/jerem/scoop/apps/win32yank/0.1.1:$PATH
+        export BROWSER=wslview
+      '';
+
+      initExtra = ''
+        # if [[ -z "$TMUX" ]]; then
+        #   tmux new-session -d -s base
+        #   eval "$TMUX"
+        #   tmux attach-session -d -t base
+        # fi
+        bindkey '^p' history-search-backward
+        bindkey '^n' history-search-forward
+        bindkey '^e' end-of-line
+        bindkey '^w' forward-word
+        bindkey "^[[3~" delete-char
+        bindkey ";5C" forward-word
+        bindkey ";5D" backward-word
+
+        zstyle ':completion:*:*:*:*:*' menu select
+
+        # Complete . and .. special directories
+        zstyle ':completion:*' special-dirs true
+
+        zstyle ':completion:*' list-colors ""
+        zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+
+        # disable named-directories autocompletion
+        zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
+
+        # Use caching so that commands like apt and dpkg complete are useable
+        zstyle ':completion:*' use-cache on
+        zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
+
+        # Don't complete uninteresting users
+        zstyle ':completion:*:*:*:users' ignored-patterns \
+                adm amanda apache at avahi avahi-autoipd beaglidx bin cacti canna \
+                clamav daemon dbus distcache dnsmasq dovecot fax ftp games gdm \
+                gkrellmd gopher hacluster haldaemon halt hsqldb ident junkbust kdm \
+                ldap lp mail mailman mailnull man messagebus  mldonkey mysql nagios \
+                named netdump news nfsnobody nobody nscd ntp nut nx obsrun openvpn \
+                operator pcap polkitd postfix postgres privoxy pulse pvm quagga radvd \
+                rpc rpcuser rpm rtkit scard shutdown squid sshd statd svn sync tftp \
+                usbmux uucp vcsa wwwrun xfs '_*'
+        # ... unless we really want to.
+        zstyle '*' single-ignored complete
+
+        # https://thevaluable.dev/zsh-completion-guide-examples/
+        zstyle ':completion:*' completer _extensions _complete _approximate
+        zstyle ':completion:*:descriptions' format '%F{green}-- %d --%f'
+        zstyle ':completion:*' group-name ""
+        zstyle ':completion:*:*:-command-:*:*' group-order alias builtins functions commands
+        zstyle ':completion:*' squeeze-slashes true
+        zstyle ':completion:*' matcher-list "" 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+        # mkcd is equivalent to takedir
+        function mkcd takedir() {
+          mkdir -p $@ && cd ''${@:$#}
+        }
+
+        function takeurl() {
+          local data thedir
+          data="$(mktemp)"
+          curl -L "$1" > "$data"
+          tar xf "$data"
+          thedir="$(tar tf "$data" | head -n 1)"
+          rm "$data"
+          cd "$thedir"
+        }
+
+        function takegit() {
+          git clone "$1"
+          cd "$(basename ''${1%%.git})"
+        }
+
+        function take() {
+          if [[ $1 =~ ^(https?|ftp).*\.(tar\.(gz|bz2|xz)|tgz)$ ]]; then
+            takeurl "$1"
+          elif [[ $1 =~ ^([A-Za-z0-9]\+@|https?|git|ssh|ftps?|rsync).*\.git/?$ ]]; then
+            takegit "$1"
+          else
+            takedir "$@"
+          fi
+        }
+
+        WORDCHARS='*?[]~=&;!#$%^(){}<>'
+
+        # fixes duplication of commands when using tab-completion
+        export LANG=C.UTF-8
+        # enables mason.nvim to work
+        export NIX_LD=$(nix eval --impure --raw --expr 'let pkgs = import <nixpkgs> {}; NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker"; in NIX_LD')
+      '';
+    };
+
+    # FIXME: This is my fish config - you can fiddle with it if you want
+    fish = {
+      enable = true;
+      # FIXME: run 'scoop install win32yank' on Windows, then add this line with your Windows username to the bottom of interactiveShellInit
+      # fish_add_path --append /mnt/c/Users/<Your Windows Username>/scoop/apps/win32yank/0.1.1
+      interactiveShellInit = ''
+               ${pkgs.any-nix-shell}/bin/any-nix-shell fish --info-right | source
+
+               ${pkgs.lib.strings.fileContents (pkgs.fetchFromGitHub {
+            owner = "rebelot";
+            repo = "kanagawa.nvim";
+            rev = "de7fb5f5de25ab45ec6039e33c80aeecc891dd92";
+            sha256 = "sha256-f/CUR0vhMJ1sZgztmVTPvmsAgp0kjFov843Mabdzvqo=";
+          }
+          + "/extras/kanagawa.fish")}
+
+               set -U fish_greeting
+        fish_add_path --append /mnt/c/Users/jerem/scoop/apps/win32yank/0.1.1
+      '';
+      functions = {
+        refresh = "source $HOME/.config/fish/config.fish";
+        take = ''mkdir -p -- "$1" && cd -- "$1"'';
+        ttake = "cd $(mktemp -d)";
+        show_path = "echo $PATH | tr ' ' '\n'";
+        posix-source = ''
+          for i in (cat $argv)
+            set arr (echo $i |tr = \n)
+            set -gx $arr[1] $arr[2]
+          end
+        '';
+      };
+      shellAbbrs =
+        {
+          gc = "nix-collect-garbage --delete-old";
+        }
+        # navigation shortcuts
+        // {
+          ".." = "cd ..";
+          "..." = "cd ../../";
+          "...." = "cd ../../../";
+          "....." = "cd ../../../../";
+        }
+        # git shortcuts
+        // {
+          gapa = "git add --patch";
+          grpa = "git reset --patch";
+          gst = "git status";
+          gdh = "git diff HEAD";
+          gp = "git push";
+          gph = "git push -u origin HEAD";
+          gco = "git checkout";
+          gcob = "git checkout -b";
+          gcm = "git checkout master";
+          gcd = "git checkout develop";
+          gsp = "git stash push -m";
+          gsa = "git stash apply stash^{/";
+          gsl = "git stash list";
+        };
+      shellAliases = {
+        jvim = "nvim";
+        lvim = "nvim";
+        pbcopy = "/mnt/c/Windows/System32/clip.exe";
+        pbpaste = "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -command 'Get-Clipboard'";
+        explorer = "/mnt/c/Windows/explorer.exe";
+      };
+      plugins = [
+        {
+          inherit (pkgs.fishPlugins.autopair) src;
+          name = "autopair";
+        }
+        {
+          inherit (pkgs.fishPlugins.done) src;
+          name = "done";
+        }
+        {
+          inherit (pkgs.fishPlugins.sponge) src;
+          name = "sponge";
+        }
+      ];
+    };
+  };
+}
